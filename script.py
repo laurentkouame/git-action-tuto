e611ddafd605333b5834b4475c9a82764400dcde
logiqueoci <logique00@gmail.com>
2023-02-23 15:41:22 +0000

    Add script file

---
+++ script.py
@@ -0,0 +1,15 @@
+# Function d'affichage du menu
+def affichermenu():
+    print("#############################################\n"
+          "###                                       ###\n"
+          "###          PROGRAMME DE GESTION         ###\n"
+          "###              DES PERSONNES            ###\n"
+          "###                                       ###\n"
+          "#############################################\n")
+
+    print("1- Enregistrer une personne")
+    print("2- Liste des personnes enregistrées")
+    print("3- Rechercher un utilisateur par sa date d'enregistrement")
+    print("4- Quitter \n")
+
+affichermenu()
